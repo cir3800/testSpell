b1d87d27f090d502761955f180140e028d96b1e6
cir3800 <cir3800@yandex.ru>
2017-02-15 02:54:15 +0300

    librarySpell, testSpell, unitTestsSpell

---
+++ test2/test2/ClassMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using librarySpell;
+
+namespace testSpell
+{
+    public class ClassMain
+    {
+        static void Main(string[] args)
+        {
+            spellClass s = new spellClass();
+            DateTime date = DateTime.Today;
+            Console.WriteLine(s.spell(date));
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.Read();
+        }
+    }
+}
